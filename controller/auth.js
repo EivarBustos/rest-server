27078f1b72d9e727e482e7b239bf0788ee2d86dc
Eivar Bustos <eivarbc@gmail.com>
2023-05-11 18:06:00 -0500

    Funcion de iniciar seccion con google creada

--- controller/auth.js
+++ controller/auth.js
@@ -5,6 +5,7 @@ const Usuario = require('../models/usuario')
 const bcryptjs = require('bcryptjs');
 
 const { generarJWT } = require('../helpers/general_jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res = response)=>{
   const {correo, password} =req.body;
@@ -45,15 +46,60 @@ const login = async(req, res = response)=>{
     //internal server error
     return res.status(500).json({
         msg: 'Hable con el administrador '
-    })
+    });
 
   }
+}
 
-   
+const googleSingIn = async(req, res = response)=>{
+    const {id_token}= req.body;
 
+    try{
+        const {nombre, img, correo}= await googleVerify(id_token);
+         //verificar si el correo ya existe 
+         let usuario = await Usuario.findOne({correo});
+         
+         //preguntar si no existe
+         if(!usuario){
+            const data ={
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google:true
+            };
+
+            usuario= new Usuario(data);
+            await usuario.save();
+         }
+         //Si el usuario en DB FALSE 
+         if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+         }
+         //generAR EL JSONE¡WEBTOKEN 
+         const token = await generarJWT(usuario.id);
+
+
+
+        res.json({
+            usuario,
+            token
+        });
+    }catch(error){
+      json.status(400).json({
+        ok:false,
+        msg: 'El token no se pudo verificar'
+      })
+    }
+
+    
 }
 
 
-module.exports={
-    login
+module.exports= {
+    login,
+    googleSingIn
+    
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,16 +2,24 @@ const {Router} = require('express');
 const {check} = require('express-validator');
 
 
-const { login } = require('../controller/auth');
+const { login, googleSingIn } = require('../controller/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
 const router = Router();
+
 router.post('/login',[
     check('correo', 'El correo es obligatorio').isEmail(),
     check('password','La contraseña es obligatoria').not().isEmpty(),
     validarCampos
-], login)
+], login),
+
+router.post('/google',[
+    check('id_token', 'El id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSingIn );
+
+
 
 
 module.exports=router;
